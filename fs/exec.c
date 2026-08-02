5d1fcabca8e311be0291e7aa62dd9f054ef043b0
undefined <hestudio@hestudio.net>
2026-08-02 14:37:47 +0800

    hestudio-sm6150-v2026.8.2

--- fs/exec.c
+++ fs/exec.c
@@ -1898,11 +1898,6 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-
-	#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat((int *)AT_FDCWD, NULL, &argv, &envp, 0);
-	#endif
-
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
 
@@ -1912,6 +1907,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
